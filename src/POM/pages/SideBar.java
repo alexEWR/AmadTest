ba114487e39b185254ed26dc246780b02008e987
AlexEwr <alex.kokhanchuk@gmail.com>
2017-04-02 23:11:19 +0300

    change methods names, refactor method in smartphones page

--- src/POM/pages/SideBar.java
+++ src/POM/pages/SideBar.java
@@ -3,14 +3,15 @@ package POM.pages;
 import configs.Config;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
+import utils.Logger;
 
 /**
- * Created by Alex on 29.03.2017.
+ * Created by Alex on 30.03.2017.
  */
 public class SideBar extends BasePage {
 
-    private static final By TELEFONY_TV_I_ELECTRONIKA_LOCATOR = By.xpath("//li[@menu_id='3361']/a");
-    private static final By HOVERED_MENU_LOCATOR = By.xpath("//nav[@class='m-main m-menu-btn-hover hover-layer']");
+    private static final By TELEPHONES_TV_AND_ELECTRONICS_LINK_LOCATOR = By.xpath("//li[@menu_id='3361']/a");
+    private static final By HOVERED_MENU_ELEMENT_LOCATOR = By.xpath("//nav[@class='m-main m-menu-btn-hover hover-layer']");
 
     public SideBar() {
         url = Config.getEnvironmentUrl();
@@ -22,16 +23,16 @@ public class SideBar extends BasePage {
         return this;
     }
 
-    public TelefonyElectronikaPage clickTelefonyTvIElectronika(){
-        getElement(TELEFONY_TV_I_ELECTRONIKA_LOCATOR).waitForVisibleAndClick(5);
-        //resolve problem of unexpected work after clicking on previous element
-        //TODO write by better way, for example add javascript executor waitForCallBacks
+    public TelephonesAndElectronicsPage clickTelephonesTvAndElectronics(){
+        Logger.STEP("Open section Telephones TV and Electronics");
+        getElement(TELEPHONES_TV_AND_ELECTRONICS_LINK_LOCATOR).waitForVisibleAndClick(5);
+        //Added for stable work of test. Resolves problem of unexpected work after clicking on previous element
         try{
-            getElement(HOVERED_MENU_LOCATOR).waitForElementVisible(2);
-            getElement(TELEFONY_TV_I_ELECTRONIKA_LOCATOR).waitForVisibleAndClick(2);
+            getElement(HOVERED_MENU_ELEMENT_LOCATOR).waitForElementVisible(2);
+            getElement(TELEPHONES_TV_AND_ELECTRONICS_LINK_LOCATOR).waitForVisibleAndClick(2);
         }catch (TimeoutException e){
-
+            Logger.EXCEPTION(e.getMessage());
         }
-        return new TelefonyElectronikaPage();
+        return new TelephonesAndElectronicsPage();
     }
 }
--- src/POM/pages/TelephonesPage.java
+++ src/POM/pages/TelephonesPage.java
@@ -1,14 +1,16 @@
 package POM.pages;
 
 import org.openqa.selenium.By;
+import utils.Logger;
 
 /**
- * Created by Alex on 29.03.2017.
+ * Created by Alex on 30.03.2017.
  */
 public class TelephonesPage extends BasePage {
     private static final By SMARTPHONES_LINK_LOCATOR = By.xpath("//li[@param='60974']//li[@class='m-cat-subl-i'][1]/a");
 
     public SmartPhonesPage clickSmartphones(){
+        Logger.STEP("Click Smartphones link");
         getElement(SMARTPHONES_LINK_LOCATOR).waitForVisibleAndClick(5);
         return new SmartPhonesPage();
     }
